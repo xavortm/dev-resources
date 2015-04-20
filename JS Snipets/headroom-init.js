e356058e8caee7d19aea5b6da0c5ce9c1c4bd6a7
Alexander Dimitrov <xavortm@gmail.com>
2015-04-20 09:46:59 +0300

    Update headroom-init.js snipped

--- JS Snipets/headroom-init.js
+++ JS Snipets/headroom-init.js
@@ -1,13 +1,27 @@
-// It will add .opened class to the WordPress default dropdown menu.
+ /**
+  * It will add .opened class to the WordPress default dropdown menu.
+  * This snipped works whitout jQuery defined.
+  *
+  * @version 0.7.0
+  * @see http://codepen.io/WickyNilliams/pen/AFsKB
+  * @CDN //cdnjs.cloudflare.com/ajax/libs/headroom/0.7.0/headroom.min.js
+  */
 
 // Scroll up-down header
 var myElement = document.querySelector(".site-header");
+
 // construct an instance of Headroom, passing the element
 var headroom  = new Headroom(myElement, {
 	offset : headerHeight,
 	onUnpin : function() {
+
+		// Hide all opened sub-menus.
 		$('.menu-item-has-children').siblings().removeClass('opened');
+
+		// Hide the opened menu after scrolling down.
+		$('.top-bar').removeClass('expanded');
 	}
 });
+
 // initialise
 headroom.init();
